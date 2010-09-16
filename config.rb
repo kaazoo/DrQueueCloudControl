c4cfc4020808944fc34f84b8e6e93fbdbdf774bc
Andreas Schroeder <andreas@drqueue.org>
2010-09-16 21:48:17 +0200

    add more config options

--- config.rb
+++ config.rb
@@ -1,11 +1,19 @@
 module DQCWconfig
 
   # sleep time for daemon
-  attr :run_interval
-  @run_interval = 10
-  
+  attr :sleep_interval
+  @sleep_interval = 10
+
+  # parking pool for waiting slaves
+  attr :parking_pool
+  @parking_pool = "garage"
+
+  # available render pool types
+  attr :pool_types
+  @pool_types = ['blender', 'maya', 'cinema4d']
+
   # DrQueue environment
   ENV['DRQUEUE_MASTER'] ||= '127.0.0.1'
   ENV['DRQUEUE_ROOT'] ||= '/usr/local/drqueue'
-  
+
 end
