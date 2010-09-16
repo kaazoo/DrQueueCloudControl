6d3f3b9c7f6d0c46b6e42566eb3ed48c92064672
Andreas Schroeder <andreas@drqueue.org>
2010-09-16 23:04:39 +0200

    added example database config options

--- config.rb
+++ config.rb
@@ -12,6 +12,30 @@ module DQCWconfig
   attr :pool_types
   @pool_types = ['blender', 'maya', 'cinema4d']
 
+  # DQCC database config
+  attr :db_dqcc_adapter
+  @db_dqcc_adapter = "mysql"
+  attr :db_dqcc_name
+  @db_dqcc_name = "drqueuecloudcontrol"
+  attr :db_dqcc_user
+  @db_dqcc_user = "dqcw"
+  attr :db_dqcc_pw
+  @db_dqcc_pw = "foobar"
+  attr :db_dqcc_host
+  @db_dqcc_host = "localhost"
+
+  # DQOR database config
+  attr :db_dqor_adapter
+  @db_dqor_adapter = "mysql"
+  attr :db_dqor_name
+  @db_dqor_name = "drqueueonrails"
+  attr :db_dqor_user
+  @db_dqor_user = "dqcw"
+  attr :db_dqor_pw
+  @db_dqor_pw = "foobar"
+  attr :db_dqor_host
+  @db_dqor_host = "localhost"
+
   # DrQueue environment
   ENV['DRQUEUE_MASTER'] ||= '127.0.0.1'
   ENV['DRQUEUE_ROOT'] ||= '/usr/local/drqueue'
