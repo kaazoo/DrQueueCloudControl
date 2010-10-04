cf4983d0e0759701c599f9eab1190a938909f5d1
Andreas Schroeder <andreas@drqueue.org>
2010-10-04 22:53:02 +0200

    add ec2 stuff

--- cloud_func.rb
+++ cloud_func.rb
@@ -1,14 +1,35 @@
 module DQCCcloud
 
+  require 'AWS'
+
+
   def start_vm
-    # foo
+    puts "DEBUG: start_vm()"
+
+    # connect to EC2
+    ec2 = AWS::EC2::Base.new(:access_key_id => ENV['AMAZON_ACCESS_KEY_ID'], :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY'])
+
+    # start new instance
+    instance_data = ec2.run_instances( {:image_id => ENV['EC2_SLAVE_AMI'], :min_count => 1, :max_count => 1, :key_name => ENV['EC2_KEY_NAME'], :user_data => nil, :instance_type => ENV['EC2_INSTANCE_TYPE'], :kernel_id => nil, :availability_zone => ENV['EC2_AVAIL_ZONE'], :base64_encoded => false, :security_group => ENV['EC2_SEC_GROUP']} )
+
+    # get instance id
+    slave = instance_data.instancesSet.item[0].instanceId
+
+    return slave
+  end
+
+
+  def stop_vm(slave)
+    puts "DEBUG: stop_vm()"
+
+    # connect to EC2
+    ec2 = AWS::EC2::Base.new(:access_key_id => ENV['AMAZON_ACCESS_KEY_ID'], :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY'])
+
+    # stop running instance
+    ec2.terminate_instances( {:instance_id => slave} )
+
     return true
   end
-  
-  def stop_vm
-    # foo
-    return true
-  end
-  
-  
+
+
 end
