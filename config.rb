15eecb349e0d575ddae38923c543b39407cb3bb7
Andreas Schroeder <andreas@drqueue.org>
2010-10-04 22:53:44 +0200

    ec2 example configuration

--- config.rb
+++ config.rb
@@ -32,4 +32,15 @@ module DQCCconfig
   ENV['DRQUEUE_MASTER'] ||= '127.0.0.1'
   ENV['DRQUEUE_ROOT'] ||= '/usr/local/drqueue'
 
+  # EC2 environment
+  ENV['AMAZON_ACCESS_KEY_ID'] ||= 'YOUR_ACCESS_KEY_ID'
+  ENV['AMAZON_SECRET_ACCESS_KEY'] ||= 'YOUR_SECRET_ACCESS_KEY_ID'
+  ENV['EC2_URL'] ||= 'https://eu-west-1.ec2.amazonaws.com'
+  ENV['EC2_SLAVE_AMI'] ||= 'ami-9999999999'
+  ENV['EC2_KEY_NAME'] ||= 'mykey'
+  ENV['EC2_INSTANCE_TYPE'] ||= 't1.micro'
+  ENV['EC2_AVAIL_ZONE'] ||= 'eu-west-1a'
+  ENV['EC2_SEC_GROUP'] ||= 'mysecgroup'
+
+
 end
