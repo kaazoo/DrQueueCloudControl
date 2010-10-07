c22f2593b3a514b2393262bea44e33abb82fdd32
Andreas Schroeder <andreas@drqueue.org>
2010-10-07 23:39:58 +0200

    fix initial VM state simplify fetch_slave_list() enhance get_parked_slaves() add method for fetching recently started VMs use chached slave list where possible fix changes on pool membership wait for recently started VMs

--- queue_func.rb
+++ queue_func.rb
@@ -23,7 +23,7 @@ module DQCCqueue
       @hostname = nil
       @public_name = nil
       @queue_info = nil
-      @state = "running"
+      @state = "pending"
     end
   end
 
@@ -49,26 +49,7 @@ module DQCCqueue
   def fetch_slave_list
     puts "DEBUG: fetch_slave_list()"
 
-    # new array
-    cl = []
-
-    0.upto(Drqueue::MAXCOMPUTERS) do |i|
-      # new computer object
-      cl[i] =  Drqueue::Computer.new
-            
-      # get computer data
-      if Drqueue::request_comp_xfer(i, cl[i], Drqueue::CLIENT) == 0
-        # no computer with that id, give up
-        cl[i] = nil
-        break
-      end
-
-      cl[i] = Drqueue::Computer.new
-    end
-    # remove all nil elements from array
-    cl.compact!
-
-    return cl
+    return Drqueue::request_computer_list(Drqueue::CLIENT)
   end
 
 
@@ -93,12 +74,6 @@ module DQCCqueue
 
     # walk through list and look for parking pool
     park_list = []
-    #slave_list = fetch_slave_list
-    #slave_list.each do |computer|
-    #  if computer.limits.pool == DQCCconfig.parking_pool
-    #    park_list << computer
-    #  end
-    #end
 
     $slave_vms.each do |vm|
       if (vm.queue_info != nil) && (vm.queue_info.limits.pool == DQCCconfig.parking_pool)
@@ -110,14 +85,31 @@ module DQCCqueue
   end
 
 
+  def get_starting_slaves
+    puts "DEBUG: get_starting_slaves()"
+
+    # walk through list and look for recently started slaves
+    starting_list = []
+
+    $slave_vms.each do |vm|
+      if (vm.state == "pending") || (vm.queue_info == nil)
+        starting_list << vm
+      end
+    end
+
+    return starting_list
+  end
+
+
   def get_user_slaves(user_hash)
     puts "DEBUG: get_user_slaves("+user_hash.to_s+")"
 
     # walk through list and look for hash in pool name
     user_list = []
-    slave_list = fetch_slave_list
-    slave_list.each do |computer|
-      if computer.limits.pool.to_s.include? user_hash
+
+    $slave_list.each do |computer|
+      puts computer_pools(computer)
+      if computer_pools(computer).include? user_hash
         user_list << computer
       end
     end
@@ -131,8 +123,7 @@ module DQCCqueue
 
     slave_info = nil
 
-    slave_list = fetch_slave_list
-    slave_list.each do |computer|
+    $slave_list.each do |computer|
       if computer.hwinfo.address == address
         slave_info = computer
         break
@@ -144,29 +135,26 @@ module DQCCqueue
   end
 
 
-  def set_slave_pool(slave_name, pool)
-    puts "DEBUG: set_slave_pool("+slave_name.to_s+", \""+pool.to_s+"\")"
+  # modify pool membership (requires direct communication to slave)
+  def set_slave_pool(slave, pool)
+    puts "DEBUG: set_slave_pool("+slave.hwinfo.name.to_s+", \""+pool.to_s+"\")"
 
-    success = false
-
-    # new array
-    cl = []
-    0.upto(Drqueue::MAXCOMPUTERS) do |i|
-      # new computer object
-      cl[i] =  Drqueue::Computer.new
-
-      # get computer data / 0 means error
-      if Drqueue::request_comp_xfer(i, cl[i], Drqueue::CLIENT) == 1
-        if cl[i].hwinfo.name == slave_name
-          Drqueue::request_job_limits_pool_set(i, pool, Drqueue::CLIENT)
-          success = true
-          break
-        end
+    # remove from all pools
+    np_max = slave.limits.npools - 1
+    (0..np_max).each do |np|
+      puts "DEBUG: removing "+slave.hwinfo.name+" from pool "+slave.limits.get_pool(np).name
+      if Drqueue::request_slave_limits_pool_remove(slave.hwinfo.name, slave.limits.get_pool(np).name, Drqueue::CLIENT) == 0
+        puts "ERROR: Could not remove slave from pool!"
       end
-
     end
 
-    return success
+    # add to specific pool
+    puts "DEBUG: adding "+slave.hwinfo.name+" to pool "+pool
+    if Drqueue::request_slave_limits_pool_add(slave.hwinfo.name, pool, Drqueue::CLIENT) == 0
+      puts "ERROR: Could not add slave to pool!"
+    end
+
+    return true
   end
 
 
@@ -185,45 +173,38 @@ module DQCCqueue
   def add_slaves(user_hash, diff)
     puts "DEBUG: add_slaves("+user_hash.to_s+", "+diff.to_s+")"
 
-    # look for unused slaves and add them to user pool(s)
-    if (parked_slaves = get_parked_slaves).length > 0
-      # work on a number of parked slaves
-      0.upto(diff - 1) do |i|
-        # add to user pool(s)
-        puts "INFO: I will add slave \""+parked_slaves[i].queue_info.name+"\" to pools \""+concat_pool_names(user_hash)+"\"."
-        set_slave_pool(parked_slaves[i].queue_info.name, concat_pool_names(user_hash))
-        # take out of park list
-        #@parked_slaves.each do |slave|
-        #  if slave.name == parked_slaves[i].name
-        #    puts slave.name
-        #    @parked_slaves.delete(slave)
-        #  end
-        #end
+    # look for slaves which have just been started
+    if (starting_slaves = get_starting_slaves).length > 0
+      usable = [starting_slaves.length, diff].min
+      puts "DEBUG: Found "+usable.to_s+" starting slaves."
+      # work on a number of starting slaves
+      0.upto(usable - 1) do |i|
+        puts "INFO: Waiting for "+starting_slaves[i].instace_id+" to finish startup."
       end
+
+    # look for unused slaves and add them to user pool(s)
+    elsif (parked_slaves = get_parked_slaves).length > 0
+      usable = [parked_slaves.length, diff].min
+      puts "DEBUG: Found "+usable.to_s+" parked slaves."
+      # work on a number of parked slaves
+      0.upto(usable - 1) do |i|
+        # add to user pool(s)
+        puts "INFO: I will add slave \""+parked_slaves[i].queue_info.hwinfo.name+"\" to pools \""+concat_pool_names(user_hash)+"\"."
+        set_slave_pool(parked_slaves[i].queue_info.hwinfo.name, concat_pool_names(user_hash))
+      end
+
     # if no free slave is running, start new one
     else
       0.upto(diff - 1) do |i|
         # start up new slave VM
-        slave = DQCCcloud.start_vm
-        
-        # sleep until VM is available
-        puts "DEBUG: Waiting for slave to arrive..."
-        sleep 25
-        
-        # add to pools
-        puts "INFO: I will add slave \""+slave.hostname+"\" to pools \""+concat_pool_names(user_hash)+"\"."
-        if set_slave_pool(slave.hostname, concat_pool_names(user_hash))
-          puts "DEBUG: pools set."
-        else
-          puts "DEBUG: pools not set!"
-        end
+        slave = DQCCcloud.start_vm(concat_pool_names(user_hash))
       end
     end
   end
 
 
   def remove_slaves(user_hash, diff)
-  puts "DEBUG: remove_slaves("+user_hash.to_s+", "+diff.to_s+")"
+    puts "DEBUG: remove_slaves("+user_hash.to_s+", "+diff.to_s+")"
 
     # work on a number of parked slaves
     if (user_slaves = get_user_slaves(user_hash)).length > 0
@@ -231,7 +212,7 @@ module DQCCqueue
         # add slaves to parking pool
         set_slave_pool(user_slaves[i], DQCCconfig.parking_pool)
         # save parking time
-        @parked_slaves << [:name => user_slaves[i].name, :parked_at => Time.now.to_i]
+        #@parked_slaves << [:name => user_slaves[i].hwinfo.name, :parked_at => Time.now.to_i]
       end
     # no user slaves found
     else
@@ -255,6 +236,22 @@ module DQCCqueue
     end
   end
 
+
+  # print list of pools
+  def computer_pools(computer)
+    puts "DEBUG: computer_pools("+computer.hwinfo.name+")"
+
+    pools = ''
+    np_max = computer.limits.npools - 1
+    (0..np_max).each do |np|
+      pools += computer.limits.get_pool(np).name
+      if np < np_max 
+        pools += ' , '
+      end
+    end
+
+    return pools 
+  end
 
 
 end
