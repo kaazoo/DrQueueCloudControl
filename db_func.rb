06a3fa0e663a4d1f4d2f47208a4a65202792aa0f
Andreas Schroeder <andreas@drqueue.org>
2012-02-08 23:08:01 +0100

    use mongoid instead of active_record add new database attributes remove payments get owner directly from rendersession instead of payment

--- db_func.rb
+++ db_func.rb
@@ -5,57 +5,73 @@ module DQCCdb
   include DQCCconfig
 
   # for database connectivity
-  require 'active_record'
+  require 'mongoid'
+
+
+  Mongoid.configure do |config|
+    name = DQCCconfig.db_dqor_name
+    host = DQCCconfig.db_dqor_host
+    config.master = Mongo::Connection.new.db(name)
+  end
+
 
   # for hash computation
   require 'digest/md5'
 
 
-  class Job < ActiveRecord::Base
-    belongs_to :profile
+  class Job
+    include Mongoid::Document
+    store_in "drqueue_jobs"
+
+    field :name, :type => String
+    field :startframe, :type => Integer
+    field :endframe, :type => Integer
+    field :blocksize, :type => Integer
+    field :renderer, :type => String
+    field :scenefile, :type => String
+    field :retries, :type => Integer
+    field :owner, :type => String
+    field :created_with, :type => String
+    field :rendertype, :type => String
+    field :send_email, :type => Boolean
+    field :email_recipients, :type => String
+    field :file_provider, :type => String
   end
 
-  class Profile < ActiveRecord::Base
-    has_many :jobs
-    has_many :payments
+  class User
+    include Mongoid::Document
+    store_in "drqueue_users"
+
+    field :name, :type => String
+    field :admin, :type => Boolean, :default => false
+    field :beta_user, :type => Boolean, :default => true
   end
 
-  class Payment < ActiveRecord::Base
-    has_many :rendersessions
-    belongs_to :profile
+  class Rendersession
+    include Mongoid::Document
+    store_in "cloudcontrol_rendersessions"
+
+    field :user, :type => String
+    field :num_slaves, :type => Integer
+    field :run_time, :type => Integer
+    field :vm_type, :type => String, :default => 't1.micro'
+    field :costs, :type => Float
+    field :paypal_token, :type => String
+    field :paypal_payer_id, :type => String
+    field :paid_at, :type => DateTime
+    field :time_passed, :type => Integer, :default => 0
+    field :start_timestamp, :type => Integer, :default => 0
+    field :stop_timestamp, :type => Integer, :default => 0
+    field :overall_time_passed, :type => Integer, :default => 0
   end
 
-  class Rendersession < ActiveRecord::Base
-    belongs_to :payment
-  end
-
-
-  # connect to DQOR database
-  def db_connect_dqor
-    ActiveRecord::Base.establish_connection(
-      :adapter  => DQCCconfig.db_dqor_adapter,
-      :database => DQCCconfig.db_dqor_name,
-      :username => DQCCconfig.db_dqor_user,
-      :password => DQCCconfig.db_dqor_pw,
-      :host     => DQCCconfig.db_dqor_host)
-  end
-
-
-  # retrieve current database connection if available
-  def db_connection
-    if ActiveRecord::Base.connected? == false
-      db_connect_dqor
-    else
-      ActiveRecord::Base.connection
-    end
-  end
 
 
   # return list of all jobs known to DQOR
   def fetch_job_list
     puts "DEBUG: fetch_job_list()"
 
-    db_connection
+    #db_connection
 
     return Job.find(:all)
   end
@@ -65,10 +81,10 @@ module DQCCdb
   def fetch_rendersession_list
     puts "DEBUG: fetch_rendersession_list()"
 
-    db_connection
-
     # fetch all paid and owned rendersessions
-    return Rendersession.find(:all, :conditions => "payment_id > 0 AND profile_id > 0")
+    sessions = Rendersession.all(:conditions => { :paid_at.ne => nil, :paypal_payer_id.ne => nil })
+    puts "DEBUG: Rendersessions found: "+sessions.length.to_s
+    return sessions
   end
 
 
@@ -76,11 +92,8 @@ module DQCCdb
   def fetch_user_data(job_id)
     puts "DEBUG: fetch_user_data("+job_id.to_s+")"
 
-    db_connection
-
     job = Job.find(job_id)
-
-    return Profile.find(job.profile_id)
+    return User.find(job.owner)
   end
 
 
@@ -88,13 +101,8 @@ module DQCCdb
   def fetch_rendersession_job_list(rendersession_id)
     puts "DEBUG: fetch_rendersession_job_list("+rendersession_id.to_s+")"
 
-    db_connection
-
     rs = Rendersession.find(rendersession_id)
-    pm = Payment.find(rs.payment_id)
-    prof = Profile.find(pm.profile_id)
-
-    return Job.find_all_by_profile_id(prof.id)
+    return Job.all(:conditions => {:owner => rs.user})
   end
 
 
@@ -102,7 +110,7 @@ module DQCCdb
   def find_rendersession(user_hash)
     puts "DEBUG: find_rendersession("+user_hash+")"
 
-    db_connection
+    #db_connection
 
     needed_pm = nil
     payments = Payment.find(:all)
