13ac8d8a201de1bf62e802cf7a8537106d5b6dc2
Andreas Schroeder <andreas@drqueue.org>
2012-02-08 22:59:20 +0100

    init rubypython use global variable for client instance

--- dqcc_daemon.rb
+++ dqcc_daemon.rb
@@ -9,6 +9,13 @@
 require 'rubygems'
 require 'daemons'
 
+# initialize rubypython
+require 'rubypython'
+RubyPython.start(:python_exe => "python2.7")
+sys = RubyPython.import "sys"
+sys.argv = [""]
+$pyDrQueue = RubyPython.import("DrQueue")
+$pyDrQueueClient = $pyDrQueue.Client.new
 
 options = {
     :app_name   => "dqcc",
