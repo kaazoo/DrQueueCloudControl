428bef2f3c4434e9ce7e3fd0bcf1704042c4b2f7
Andreas Schroeder <andreas@drqueue.org>
2012-03-11 15:28:57 +0100

    make code more readable

--- dqcc.rb
+++ dqcc.rb
@@ -24,11 +24,13 @@ require 'queue_func'
 include DQCCqueue
 
 
+puts "DEBUG MEMORY Linux: run \"watch pmap #{Process.pid} | tail -n 1\" in another terminal."
+puts "DEBUG MEMORY OSX: run \"watch vmmap #{Process.pid} | tail -n 12\" in another terminal."
+
+
 # sleep a while
 def tea_break
   puts "\n* waiting a while"
-  puts "DEBUG MEMORY Linux: run \"watch pmap #{Process.pid} | tail -n 1\" in another terminal."
-  puts "DEBUG MEMORY OSX: run \"watch vmmap #{Process.pid} | tail -n 12\" in another terminal."
   sleep DQCCconfig.sleep_interval
 end
 
@@ -49,17 +51,18 @@ loop do
 
     # skip this rendersession if not used
     if job_list.length == 0
-      puts "INFO: Rendersession "+rs.id.to_s+" isn't in use yet. Skipping this one."
+      puts "INFO: Rendersession " + rs.id.to_s + " isn't in use yet. Skipping this one."
+      # skip to next session
       next
     end
 
     running_jobs = []
 
-    # fetch queue info for each job and check status
+    # fetch info for each job and check status
     job_list.each do |job|
       job_info = DQCCqueue.fetch_job_info(job.id)
       if job_info == nil
-        puts "ERROR: Queue info for job "+job.id.to_s+" could not be fetched."
+        puts "ERROR: Queue info for job " + job.id.to_s + " could not be fetched."
       else
         # see if there is any job active or waiting
         if DQCCqueue.job_status(job.id) == "pending"
@@ -72,40 +75,40 @@ loop do
 
     # remove eventually running slaves of this session
     if running_jobs.length == 0
-      puts "INFO: There are no running jobs in rendersession "+rs.id.to_s+". Removing all slaves if any."
-      # remove all slaves
+      puts "INFO: There are no running jobs in rendersession " + rs.id.to_s + ". Removing all slaves if any."
+      # remove all slaves of this rendersession
       DQCCqueue.remove_slaves(rs.user, rs.vm_type, rs.num_slaves)
       # update time counter
       if rs.stop_timestamp == 0
         rs.overall_time_passed += rs.time_passed
         rs.time_passed = 0
         rs.stop_timestamp = Time.now.to_i
-        puts "INFO: Setting stop timestamp to: "+rs.stop_timestamp.to_s+"."
+        puts "INFO: Setting stop timestamp to: " + rs.stop_timestamp.to_s + "."
         rs.save!
       end
       # skip to next session
       next
     else
-      puts "INFO: There are "+running_jobs.length.to_s+" running jobs in rendersession "+rs.id.to_s+"."
+      puts "INFO: There are " + running_jobs.length.to_s + " running jobs in rendersession " + rs.id.to_s + "."
     end
 
     # look if there is time left (in seconds)
     if (time_left = rs.run_time * 3600 - (rs.overall_time_passed + rs.time_passed)) > 0
-      puts "INFO: There are "+time_left.to_s+" sec left in session "+rs.id.to_s+"."
+      puts "INFO: There are " + time_left.to_s + " sec left in session " + rs.id.to_s + "."
       # check if slaves are running
       running_slaves = DQCCqueue.get_running_user_slaves(rs.vm_type, rs.user).length
       puts "DEBUG: There are " + running_slaves.to_s + " slaves already running."
       diff = rs.num_slaves - running_slaves
       max_diff = DQCCconfig.max_vms - $slave_vms.length
       if diff > max_diff
-        puts "ERROR: Requested number of slaves exceeds maximum number of VMs. Will only add "+max_diff.to_s+" slaves."
+        puts "ERROR: Requested number of slaves exceeds maximum number of VMs. Will only add " + max_diff.to_s + " slaves."
         DQCCqueue.add_slaves(rs.user, rs.vm_type, max_diff)
       elsif diff > 0
-        puts "INFO: I have to add "+diff.to_s+" more slaves to session "+rs.id.to_s+"."
+        puts "INFO: I have to add " + diff.to_s + " more slaves to session " + rs.id.to_s + "."
         # add slaves
         DQCCqueue.add_slaves(rs.user, rs.vm_type, diff)
       elsif diff < 0
-        puts "INFO: I have to remove "+diff.abs.to_s+" slaves from session "+rs.id.to_s+"."
+        puts "INFO: I have to remove " + diff.abs.to_s + " slaves from session " + rs.id.to_s + "."
         # remove slaves because there are more then defined
         DQCCqueue.remove_slaves(rs.user, rs.vm_type, diff.abs)
       else
@@ -122,23 +125,23 @@ loop do
         # continue counting when a job is active again
         rs.start_timestamp = Time.now.to_i
         rs.stop_timestamp = 0
-        otp_hours = (rs.overall_time_passed/3600).to_i
-        otp_minutes = (rs.overall_time_passed/60 - otp_hours * 60).to_i
+        otp_hours = (rs.overall_time_passed / 3600).to_i
+        otp_minutes = (rs.overall_time_passed / 60 - otp_hours * 60).to_i
         otp_seconds = (rs.overall_time_passed - (otp_minutes * 60 + otp_hours * 3600)).to_i
-        puts "INFO: Session continues. Overall time passed: "+"%02d"%otp_hours+":"+"%02d"%otp_minutes+":"+"%02d"%otp_seconds
+        puts "INFO: Session continues. Overall time passed: " + "%02d" % otp_hours + ":" + "%02d" % otp_minutes + ":" + "%02d" % otp_seconds
       else
         # update time counter
         rs.time_passed = Time.now.to_i - rs.start_timestamp
-        otp_hours = (rs.overall_time_passed/3600).to_i
-        otp_minutes = (rs.overall_time_passed/60 - otp_hours * 60).to_i
+        otp_hours = (rs.overall_time_passed / 3600).to_i
+        otp_minutes = (rs.overall_time_passed / 60 - otp_hours * 60).to_i
         otp_seconds = (rs.overall_time_passed - (otp_minutes * 60 + otp_hours * 3600)).to_i
-        puts "INFO: Time passed: "+rs.time_passed.to_s+" sec. Overall time passed: "+"%02d"%otp_hours+":"+"%02d"%otp_minutes+":"+"%02d"%otp_seconds
+        puts "INFO: Time passed: " + rs.time_passed.to_s + " sec. Overall time passed: " + "%02d" % otp_hours + ":" + "%02d" % otp_minutes + ":" + "%02d" % otp_seconds
       end
       rs.save!
 
     # no time is left in the session
     else
-      puts "INFO: No time left. I have to remove all slaves from session "+rs.id.to_s+"."
+      puts "INFO: No time left. I have to remove all slaves from session " + rs.id.to_s + "."
       # remove all slaves
       DQCCqueue.remove_slaves(rs.user, rs.vm_type, rs.num_slaves)
       # update time counter
