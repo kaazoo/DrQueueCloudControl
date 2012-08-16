e38e9e530c57ac3c47ef22cce74257b22e48c37f
Andreas Schroeder <andreas@drqueue.org>
2012-08-17 00:59:45 +0200

    use MongoEngine instead of MiniMongo use central config

--- db_func.py
+++ db_func.py
@@ -1,137 +1,91 @@
+# for database connectivity
+from mongoengine import *
+
+# Job objects stored in MongoDB
+class Job(Document):
+    meta = {
+        "collection": "drqueue_jobs",
+        "allow_inheritance": False,
+    }
+    name = StringField()
+    startframe = IntField()
+    endframe = IntField()
+    blocksize = IntField()
+    renderer = StringField()
+    scenefile = StringField()
+    retries = IntField()
+    owner = StringField()
+    created_with = StringField()
+    rendertype = StringField()
+    send_email = BooleanField()
+    email_recipients = StringField()
+    file_provider = StringField()
+
+
+# User objects stored in MongoDB
+class User(Document):
+    meta = {
+        "collection": "drqueue_users",
+        "allow_inheritance": False,
+    }
+    name = StringField()
+    admin = BooleanField(default=False)
+    beta_user = BooleanField(default=True)
+
+
+# Rendersession objects stored in MongoDB
+class Rendersession(Document):
+    meta = {
+        "collection": "cloudcontrol_rendersessions",
+        "allow_inheritance": False,
+    }
+    user = StringField()
+    num_slaves = IntField()
+    run_time = IntField()
+    vm_type = StringField(default="t1.micro")
+    costs = FloatField()
+    active = BooleanField()
+    paypal_token = StringField()
+    paypal_payer_id = StringField()
+    paid_at = DateTimeField()
+    time_passed = IntField(default=0)
+    start_timestamp = IntField(default=0)
+    stop_timestamp = IntField(default=0)
+    overall_time_passed = IntField(default=0)
+
+
 class DQCCdb():
 
-    # for database connectivity
-    import minimongo
-
-    # read config from config file
-    import ConfigParser
-    config = ConfigParser.RawConfigParser()
-    config.read("dqcc.cfg")
-
-    # store config
-    global db_config
-    db_config = {}
-    db_config["db_dqor_host"] = config.get("DQCCconfig", "db_dqor_host")
-    db_config["db_dqor_name"] = config.get("DQCCconfig", "db_dqor_name")
+    # config shared accross all modules/classes
+    import config as DQCCconfig
 
     # debug config
     print("\nDB configuration:")
-    print(db_config["db_dqor_host"])
-    print(db_config["db_dqor_name"])
+    print("db_dqor_host = " + DQCCconfig.db_dqor_host)
+    print("db_dqor_name = " + DQCCconfig.db_dqor_name)
 
-
-    class Job(minimongo.Model):
-        class Meta:
-            # Here, we specify the database and collection names.
-            # A connection to your DB is automatically created.
-            global db_config
-            hostname = db_config["db_dqor_host"]
-            database = db_config["db_dqor_name"]
-            collection = "drqueue_jobs"
-
-            # Now, we programatically declare what indices we want.
-            # The arguments to the Index constructor are identical to
-            # the args to pymongo"s ensure_index function.
-            #indices = (
-            #    minimongo.Index("name"),
-            #)
-
-        #include Mongoid::Document
-        #store_in "drqueue_jobs"
-
-#    field :name, :type => String
-#    field :startframe, :type => Integer
-#    field :endframe, :type => Integer
-#    field :blocksize, :type => Integer
-#    field :renderer, :type => String
-#    field :scenefile, :type => String
-#    field :retries, :type => Integer
-#    field :owner, :type => String
-#    field :created_with, :type => String
-#    field :rendertype, :type => String
-#    field :send_email, :type => Boolean
-#    field :email_recipients, :type => String
-#    field :file_provider, :type => String
-
-
-    class User(minimongo.Model):
-        class Meta:
-            # Here, we specify the database and collection names.
-            # A connection to your DB is automatically created.
-            global db_config
-            hostname = db_config["db_dqor_host"]
-            database = db_config["db_dqor_name"]
-            collection = "drqueue_users"
-
-            # Now, we programatically declare what indices we want.
-            # The arguments to the Index constructor are identical to
-            # the args to pymongo"s ensure_index function.
-            #indices = (
-            #    minimongo.Index("name"),
-            #)
-
-
-#    field :name, :type => String
-#    field :admin, :type => Boolean, :default => false
-#    field :beta_user, :type => Boolean, :default => true
-
-
-    class Rendersession(minimongo.Model):
-        class Meta:
-            # Here, we specify the database and collection names.
-            # A connection to your DB is automatically created.
-            global db_config
-            hostname = db_config["db_dqor_host"]
-            database = db_config["db_dqor_name"]
-            collection = "cloudcontrol_rendersessions"
-
-            # Now, we programatically declare what indices we want.
-            # The arguments to the Index constructor are identical to
-            # the args to pymongo"s ensure_index function.
-            #indices = (
-            #    minimongo.Index("name"),
-            #)
-
-
-#    field :user, :type => String
-#    field :num_slaves, :type => Integer
-#    field :run_time, :type => Integer
-#    field :vm_type, :type => String, :default => 't1.micro'
-#    field :costs, :type => Float
-#    field :active, :type => Boolean
-#
-#    field :paypal_token, :type => String
-#    field :paypal_payer_id, :type => String
-#    field :paid_at, :type => DateTime
-#
-#    field :time_passed, :type => Integer, :default => 0
-#    field :start_timestamp, :type => Integer, :default => 0
-#    field :stop_timestamp, :type => Integer, :default => 0
-#    field :overall_time_passed, :type => Integer, :default => 0
-
-
+    # connect to DB
+    try:
+        connect(DQCCconfig.db_dqor_name, host=DQCCconfig.db_dqor_host)
+    except ConnectionError:
+        print("\nCould not connect to MongoDB on host '" + DQCCconfig.db_dqor_host + "'! Check value of 'db_dqor_host' in dqcc.cfg.")
+        exit(1)
 
 
     # return list of all rendersessions
     @staticmethod
     def fetch_rendersession_list():
         print("DEBUG: fetch_rendersession_list()")
-
         # fetch all paid, owned and active rendersessions
-        sessions = Rendersession.collection.find_one({"paid_at.ne": None, "paypal_payer_id.ne": None, "active": True})
-        #sessions = Rendersession.all(:conditions => { :paid_at.ne => nil, :paypal_payer_id.ne => nil, :active => true })
-
-        print("DEBUG: Rendersessions found: "+sessions.length.to_s)
+        sessions = Rendersession.objects(paid_at__ne=None, paypal_payer_id__ne=None, active=True)
+        print("DEBUG: Rendersessions found: " + str(len(sessions)))
         return sessions
 
 
     # return list of jobs belonging to a rendersession
     @staticmethod
-    def fetch_rendersession_job_list(rendersession_id):
-        print("DEBUG: fetch_rendersession_job_list("+rendersession_id.to_s+")")
-
-        rs = Rendersession.collection.find_one({"id": rendersession_id})
-        #rs = Rendersession.find(rendersession_id)
-        jobs = Jobs.collection.find_one({"owner": rs.user})
-        #jobs = Job.all(:conditions => {:owner => rs.user})
+    def fetch_rendersession_job_list(rendersession):
+        print("DEBUG: fetch_rendersession_job_list(" + str(rendersession) + ")")
+        # search for all jobs of the rendersession's owner
+        jobs = Job.objects(owner=rendersession.user)
         return jobs
