f341559663b254662678ce169cb5011f6f8713c3
Andreas Schroeder <andreas@drqueue.org>
2012-08-17 00:38:50 +0200

    parse all config variables from cfg file & store their values in central config module

--- dqcc.py
+++ dqcc.py
@@ -17,11 +17,60 @@ import DrQueue
 from DrQueue import Job as DrQueueJob
 from DrQueue import Client as DrQueueClient
 
-# initialize DrQueue client
-client = DrQueueClient()
+# runtime config shared accross all modules/classes
+import config as DQCCconfig
 
-# config
-from config import DQCCconfig
+# initialize DrQueue client
+DQCCconfig.client = DrQueueClient()
+
+# read configuration from config file
+import ConfigParser
+config = ConfigParser.RawConfigParser()
+config.read("dqcc.cfg")
+
+DQCCconfig.sleep_interval = config.getint("DQCCconfig", "sleep_interval")
+DQCCconfig.stop_behaviour = config.get("DQCCconfig", "stop_behaviour")
+DQCCconfig.parking_pool = config.get("DQCCconfig", "parking_pool")
+DQCCconfig.park_time = config.getint("DQCCconfig", "park_time")
+DQCCconfig.max_vms = config.getint("DQCCconfig", "max_vms")
+DQCCconfig.pool_types = config.get("DQCCconfig", "pool_types")
+DQCCconfig.cache_time = config.getint("DQCCconfig", "cache_time")
+DQCCconfig.testmode = config.getboolean("DQCCconfig", "testmode")
+DQCCconfig.max_wait = config.getint("DQCCconfig", "max_wait")
+DQCCconfig.ebs_encryption_salt = config.get("DQCCconfig", "ebs_encryption_salt")
+DQCCconfig.db_dqor_name = config.get("DQCCconfig", "db_dqor_name")
+DQCCconfig.db_dqor_host = config.get("DQCCconfig", "db_dqor_host")
+DQCCconfig.ec2_slave_ami = config.get("DQCCconfig", "ec2_slave_ami")
+DQCCconfig.ec2_key_name = config.get("DQCCconfig", "ec2_key_name")
+DQCCconfig.ec2_instance_type = config.get("DQCCconfig", "ec2_instance_type")
+DQCCconfig.ec2_region = config.get("DQCCconfig", "ec2_region")
+DQCCconfig.ec2_avail_zone = config.get("DQCCconfig", "ec2_avail_zone")
+DQCCconfig.ec2_sec_group = config.get("DQCCconfig", "ec2_sec_group")
+DQCCconfig.ec2_access_key_id = config.get("DQCCconfig", "ec2_access_key_id")
+DQCCconfig.ec2_secret_access_key = config.get("DQCCconfig", "ec2_secret_access_key")
+
+# debug config file
+print("\nMain configuration:")
+print("sleep_interval = " + str(DQCCconfig.sleep_interval))
+print("stop_behaviour = " + DQCCconfig.stop_behaviour)
+print("parking_pool = " + DQCCconfig.parking_pool)
+print("park_time = " + str(DQCCconfig.park_time))
+print("max_vms = " + str(DQCCconfig.max_vms))
+print("pool_types = " + DQCCconfig.pool_types)
+print("cache_time = " + str(DQCCconfig.cache_time))
+print("testmode = " + str(DQCCconfig.testmode))
+print("max_wait = " + str(DQCCconfig.max_wait))
+print("ebs_encryption_salt = " + DQCCconfig.ebs_encryption_salt)
+print("db_dqor_name = " + DQCCconfig.db_dqor_name)
+print("db_dqor_host = " + DQCCconfig.db_dqor_host)
+print("ec2_slave_ami = " + DQCCconfig.ec2_slave_ami)
+print("ec2_key_name = " + DQCCconfig.ec2_key_name)
+print("ec2_instance_type = " + DQCCconfig.ec2_instance_type)
+print("ec2_avail_zone = " + DQCCconfig.ec2_avail_zone)
+print("ec2_sec_group = " + DQCCconfig.ec2_sec_group)
+print("ec2_access_key_id = " + DQCCconfig.ec2_access_key_id)
+print("ec2_secret_access_key = " + DQCCconfig.ec2_secret_access_key + "\n")
+
 
 # database functionality
 from db_func import DQCCdb
@@ -32,37 +81,6 @@ from queue_func import DQCCqueue
 # cloud functionality
 from cloud_func import DQCCcloud
 
-# read config from config file
-import ConfigParser
-config = ConfigParser.RawConfigParser()
-config.read("dqcc.cfg")
-
-# debug config file
-print(config.getint("DQCCconfig", "sleep_interval"))
-print(config.get("DQCCconfig", "stop_behaviour"))
-print(config.get("DQCCconfig", "parking_pool"))
-print(config.getint("DQCCconfig", "park_time"))
-print(config.getint("DQCCconfig", "max_vms"))
-print(config.get("DQCCconfig", "pool_types"))
-print(config.getint("DQCCconfig", "cache_time"))
-print(config.getboolean("DQCCconfig", "testmode"))
-print(config.getint("DQCCconfig", "max_wait"))
-print(config.get("DQCCconfig", "ebs_encryption_salt"))
-print(config.get("DQCCconfig", "db_dqor_name"))
-print(config.get("DQCCconfig", "db_dqor_host"))
-print(config.get("DQCCconfig", "ec2_slave_ami"))
-print(config.get("DQCCconfig", "ec2_key_name"))
-print(config.get("DQCCconfig", "ec2_instance_type"))
-print(config.get("DQCCconfig", "ec2_avail_zone"))
-print(config.get("DQCCconfig", "ec2_sec_group"))
-
-
-# initialize global variables
-global slave_list
-global slave_vms
-slave_list = None
-slave_vms = None
-
 
 # sleep a while
 def tea_break():
@@ -72,22 +90,22 @@ def tea_break():
 
 # main daemon loop
 while(True):
-    # tea_break()
+    tea_break()
 
     # fetch running slaves, registered in DrQueue and EC2
-    DQCCqueue.slave_list = client.query_computer_list()
-    slave_vms = DQCCcloud.get_slave_vms()
+    DQCCconfig.slave_list = DQCCconfig.client.query_computer_list()
+    DQCCconfig.slave_vms = DQCCcloud.get_slave_vms()
 
     # cycle through all DQOR rendersessions
     rs_list = DQCCdb.fetch_rendersession_list()
     for rs in rs_list:
 
         # fetch list of all belonging jobs
-        job_list = DQCCdb.fetch_rendersession_job_list(rs.id)
+        job_list = DQCCdb.fetch_rendersession_job_list(rs)
 
         # skip this rendersession if not used
         if len(job_list) == 0:
-            print("INFO: Rendersession " + rs.id.to_s + " isn't in use yet. Skipping this one.")
+            print("INFO: Rendersession " + str(rs.id) + " isn't in use yet. Skipping this one.")
             # skip to next session
             next
 
@@ -97,7 +115,7 @@ while(True):
         for job in job_list:
             job_info = DQCCqueue.fetch_job_info(job.id)
             if job_info == None:
-                print("ERROR: Queue info for job " + job.id.to_s + " could not be fetched.")
+                print("ERROR: Queue info for job " + str(job.id) + " could not be fetched.")
             else:
                 # see if there is any job active or waiting
                 if DQCCqueue.job_status(job.id) == "pending":
@@ -107,7 +125,7 @@ while(True):
 
         # remove eventually running slaves of this session
         if len(running_jobs) == 0:
-            print("INFO: There are no running jobs in rendersession " + rs.id.to_s + ". Removing all slaves if any.")
+            print("INFO: There are no running jobs in rendersession " + str(rs.id) + ". Removing all slaves if any.")
             # remove all slaves of this rendersession
             DQCCqueue.remove_slaves(rs.user, rs.vm_type, rs.num_slaves)
             # update time counter
@@ -115,12 +133,12 @@ while(True):
                 rs.overall_time_passed += rs.time_passed
                 rs.time_passed = 0
                 rs.stop_timestamp = Time.now.to_i
-                print("INFO: Setting stop timestamp to: " + rs.stop_timestamp.to_s + ".")
+                print("INFO: Setting stop timestamp to: " + str(rs.stop_timestamp) + ".")
                 rs.save()
             # skip to next session
             next
         else:
-            print("INFO: There are " + running_jobs.length.to_s + " running jobs in rendersession " + rs.id.to_s + ".")
+            print("INFO: There are " + str(len(running_jobs)) + " running jobs in rendersession " + str(rs.id) + ".")
 
         # look if there is time left (in seconds)
         time_left = rs.run_time * 3600 - (rs.overall_time_passed + rs.time_passed)
@@ -128,9 +146,9 @@ while(True):
             print("INFO: There are " + str(time_left) + " sec left in session " + str(rs.id) + ".")
             # check if slaves are running
             running_slaves = len(DQCCqueue.get_running_user_slaves(rs.vm_type, rs.user))
-            print("DEBUG: There are " + running_slaves.to_s + " slaves already running.")
+            print("DEBUG: There are " + str(running_slaves) + " slaves already running.")
             diff = rs.num_slaves - running_slaves
-            max_diff = DQCCconfig.max_vms - len(slave_vms)
+            max_diff = DQCCconfig.max_vms - len(DQCCconfig.slave_vms)
             if diff > max_diff:
                 print("ERROR: Requested number of slaves exceeds maximum number of VMs. Will only add " + str(max_diff) + " slaves.")
                 DQCCqueue.add_slaves(rs.user, rs.vm_type, max_diff)
