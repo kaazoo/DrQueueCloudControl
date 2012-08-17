598ec1175509bdc37b76cb30bbafdf39df0f22a3
Andreas Schroeder <andreas@drqueue.org>
2012-08-17 08:33:19 +0200

    use central config & fix string output & make methods static

--- queue_func.py
+++ queue_func.py
@@ -1,61 +1,47 @@
 class DQCCqueue():
 
-    # config
-    from config import DQCCconfig
+    # config shared accross all modules/classes
+    global DQCCconfig
+    import config as DQCCconfig
 
     # cloud functionality
     from cloud_func import DQCCcloud
 
-    global client
-    global slave_vms
-    global slave_list
-
-
-    class SlaveVM():
-        #attr_accessor :instance_id, :instance_type, :owner, :hostname, :public_dns, :private_dns, :private_ip, :vpn_ip, :queue_info, :state, :parked_at, :pool_name_list, :launch_time
-
-        def __init__(self, instance_id, instance_type, owner):
-            self.instance_id = instance_id
-            self.instance_type = instance_type
-            self.owner = owner
-            self.hostname = nil
-            self.public_dns = nil
-            self.private_dns = nil
-            self.private_ip = nil
-            self.vpn_ip = nil
-            self.queue_info = nil
-            self.state = "pending"
-            self.parked_at = nil
-            self.pool_name_list = nil
-            self.launch_time = nil
+    # SlaveVM class definition
+    from slave_vm import SlaveVM
 
 
     # return DrQueue job information of a job
+    @staticmethod
     def fetch_job_info(job_id):
-        print("DEBUG: fetch_job_info("+job_id.to_s+")")
-        return client.query_job_by_id(job_id.to_s)
+        #global DQCCconfig
+        print("DEBUG: fetch_job_info(" + str(job_id) +")")
+        return DQCCconfig.client.query_job_by_id(str(job_id))
 
 
     # return status of job
+    @staticmethod
     def job_status(job_id):
-        print("DEBUG: job_status("+job_id.to_s+")")
-        return client.job_status(job_id.to_s)
+        print("DEBUG: job_status(" + str(job_id) + ")")
+        return DQCCconfig.client.job_status(str(job_id))
 
 
     # return list of slaves known to DrQueue master
+    @staticmethod
     def fetch_slave_list():
         print("DEBUG: fetch_slave_list()")
-        return client.query_computer_list()
+        return DQCCconfig.client.query_computer_list()
 
 
     # return list of all currently parked slaves
+    @staticmethod
     def get_all_parked_slaves():
         print("DEBUG: get_all_parked_slaves()")
 
         # walk through list and look for parking pool
         parked_list = []
 
-        for vm in slave_vms:
+        for vm in DQCCconfig.slave_vms:
             if (vm.queue_info != None):
                 for pool in vm.pool_name_list:
                     if pool.find(DQCCconfig.parking_pool):
@@ -66,40 +52,43 @@ class DQCCqueue():
 
 
     # return list of all currently starting slaves belong to a user
+    @staticmethod
     def get_starting_user_slaves(vm_type, owner):
-        print("DEBUG: get_starting_user_slaves("+vm_type.to_s+", "+owner.to_s+")")
+        print("DEBUG: get_starting_user_slaves(" + str(vm_type) + ", " + str(owner) +")")
 
         # walk through list and look for recently started slaves
         starting_list = []
 
-        for vm in slave_vms:
+        for vm in DQCCconfig.slave_vms:
             # newly created VMs can be pending (booting VM)
             # OR
             # running but without queue_info (rendering job right after start OR not having DrQueue slave process started yet)
             if ( (vm.state == "pending") and (vm.instance_type == vm_type) ) or ( (vm.state == "running") and (vm.instance_type == vm_type) and (vm.queue_info == nil) ):
                 starting_list.append(vm)
 
-        print("DEBUG: Found " + starting_list.length.to_s + " starting slaves.")
+        print("DEBUG: Found " + str(len(starting_list)) + " starting slaves.")
         return starting_list
 
 
     # return list of running slave VMs belonging to a user
+    @staticmethod
     def get_running_user_slaves(vm_type, owner):
-        print("DEBUG: get_running_user_slaves(" + vm_type.to_s + ", " + owner.to_s + ")")
+        print("DEBUG: get_running_user_slaves(" + str(vm_type) + ", " + str(owner) + ")")
 
         # walk through list and look for user_id in pool names
         running_list = []
 
-        for vm in slave_vms:
+        for vm in DQCCconfig.slave_vms:
             # slave has to be in any pool which doesn't contain parking pool name and but belongs to user
             if (vm.pool_name_list != nil) and (vm.pool_name_list.find(owner + "_" + DQCCconfig.parking_pool) == False) and (vm.instance_type == vm_type) and (vm.owner == owner):
                 running_list.append(vm)
 
-        print("DEBUG: Found " + running_list.length.to_s + " running slaves.")
+        print("DEBUG: Found " + str(len(running_list)) + " running slaves.")
         return running_list
 
 
     # return list of parked slave VMs belonging to a user
+    @staticmethod
     def get_parked_user_slaves(vm_type, owner):
         print("DEBUG: get_parked_user_slaves(" + vm_type.to_s + ", " + owner.to_s + ")")
 
@@ -116,6 +105,7 @@ class DQCCqueue():
 
 
     # return DrQueue info of computer by it's IP address
+    @staticmethod
     def get_slave_info(address):
         print("DEBUG: get_slave_info(" + address.to_s + ")")
 
@@ -125,7 +115,7 @@ class DQCCqueue():
         slave_info = nil
 
         for computer in slave_list:
-            comp = client.identify_computer(computer, DQCCconfig.cache_time)
+            comp = DQCCconfig.client.identify_computer(computer, DQCCconfig.cache_time)
             if (comp != None) and (comp['address'].to_s == address):
                 slave_info = comp
                 break
@@ -133,6 +123,7 @@ class DQCCqueue():
 
 
     # return user of a slave by it's poolnames
+    @staticmethod
     def get_owner_from_pools(slave):
         print("DEBUG: get_owner_from_pools(" + slave.hostname.to_s + ")")
 
@@ -148,31 +139,34 @@ class DQCCqueue():
 
 
     # modify pool membership
+    @staticmethod
     def set_slave_pool(slave, pool):
         print("DEBUG: set_slave_pool(" + slave.hostname.to_s + ", \"" + pool.to_s + "\")")
         print("DEBUG: adding " + slave.hostname.to_s + " to pool " + pool.to_s)
         # update pool membership in DrQueue
-        client.computer_set_pools(slave.queue_info, pool.split(","))
+        DQCCconfig.client.computer_set_pools(slave.queue_info, pool.split(","))
         # update SlaveVM object
         slave.pool_name_list = pool
         return true
 
 
     # add a number of slaves which belong to a user and match a special type
+    @staticmethod
     def add_slaves(user_id, vm_type, diff):
-        print("DEBUG: add_slaves(" + user_id.to_s + ", " + vm_type.to_s + ", " + diff.to_s + ")")
+        print("DEBUG: add_slaves(" + str(user_id) + ", " + str(vm_type) + ", " + str(diff) + ")")
 
         remaining = diff
 
         # look for slaves which have just been started
-        if len(starting_slaves = get_starting_user_slaves(vm_type, user_id)) > 0:
+        starting_slaves = DQCCqueue.get_starting_user_slaves(vm_type, user_id)
+        if len(starting_slaves) > 0:
             usable = [starting_slaves.length, remaining].min
-            print("DEBUG: Found "+usable.to_s+" starting slaves of type \""+vm_type+"\".")
+            print("DEBUG: Found " + str(usable) + " starting slaves of type \"" + vm_type + "\".")
             # work on a number of starting slaves
             for i in range(0, (usable - 1)):
                 print("INFO: Waiting for "+starting_slaves[i].instance_id+" to finish startup.")
-        if remaining >= usable:
-            remaining -= usable
+            if remaining >= usable:
+                remaining -= usable
             print("DEBUG: Remaining slaves that need to be started: " + str(remaining))
 
         # reuse park slaves if configured
@@ -197,12 +191,13 @@ class DQCCqueue():
         if remaining > 0:
             for i in range(0, (remaining - 1)):
                 # start up new slave VM
-                slave = DQCCcloud.start_vm(user_id, vm_type, concat_pool_names_of_user(user_id))
+                slave = cloud_func.start_vm(user_id, vm_type, concat_pool_names_of_user(user_id))
                 if slave == None:
                     print("ERROR: Failed to start VM.")
 
 
     # remove a number of slaves which belong to a user and match a special type
+    @staticmethod
     def remove_slaves(user_id, vm_type, diff):
         print("DEBUG: remove_slaves(" + user_id.to_s + ", " + vm_type.to_s + ", " + diff.to_s + ")")
 
@@ -223,7 +218,7 @@ class DQCCqueue():
                 # shutdown slaves immediately
                 elif DQCCconfig.stop_behaviour == "shutdown":
                     # stop slave VM
-                    DQCCcloud.stop_vm(vm)
+                    cloud_func.stop_vm(vm)
                     # remove from global list
                     slave_vms.delete(vm)
                 # shutdown slaves only 5 minutes before next full hour
@@ -235,7 +230,7 @@ class DQCCqueue():
                     if seconds_to_next_hour <= 300:
                         print("DEBUG: There are " + seconds_to_next_hour.to_s + " seconds until the next full hour. Will stop instance " + vm.instance_id + " now.")
                         # stop slave VM
-                        DQCCcloud.stop_vm(vm)
+                        cloud_func.stop_vm(vm)
                         # remove from global list
                         slave_vms.delete(vm)
                     else:
@@ -256,7 +251,7 @@ class DQCCqueue():
             if slave.parked_at == None:
                 print("ERROR: Slave "+slave.instance_id+" has been parked but when isn't known. Shutting down now.")
                 # stop slave VM
-                DQCCcloud.stop_vm(slave)
+                cloud_func.stop_vm(slave)
                 # remove from global list
                 slave_vms.delete(slave)
             else:
@@ -264,7 +259,7 @@ class DQCCqueue():
                 if (int(Time.now) - DQCCconfig.park_time) > slave.parked_at:
                     print("INFO: Slave "+slave.instance_id+" has been parked at "+Time.at(slave.parked_at).to_s+" and will be shut down now.")
                     # stop slave VM
-                    DQCCcloud.stop_vm(slave)
+                    cloud_func.stop_vm(slave)
                     # remove from global list
                     slave_vms.delete(slave)
                 else:
@@ -278,7 +273,7 @@ class DQCCqueue():
         if slave == None:
           return []
 
-        return client.computer_get_pools(slave.queue_info)
+        return DQCCconfig.client.computer_get_pools(slave.queue_info)
 
 
     # concat all possible poolnames for a user
